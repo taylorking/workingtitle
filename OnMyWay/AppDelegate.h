b41ce964514870a33a28775981609f4cf209081e
Taylor King <kingtb@appstate.edu>
2015-08-25 08:58:30 -0400

    Initial Commit

---
+++ OnMyWay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnMyWay
+//
+//  Created by Taylor King on 8/25/15.
+//  Copyright (c) 2015 omwltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnMyWay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnMyWay
+//
+//  Created by Taylor King on 8/25/15.
+//  Copyright (c) 2015 omwltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
